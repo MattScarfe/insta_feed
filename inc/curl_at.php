50c6742d308adabbde3301d95dae580d01a45d04
MattScarfe <matt.scarfe@outlook.com>
2018-12-18 17:17:55 +0000

    Seperate CURL Script

---
+++ inc/curl_at.php
@@ -0,0 +1,34 @@
+	<?php
+	
+	if (isset($_GET['code'])) {
+				$code = "'"$_GET['code']"'";
+	} else {
+				echo "Error-Parameter not set.";
+	}
+
+	
+	/// curl with PHP
+	$uri = 'https://api.instagram.com/oauth/access_token'; 
+	$data = [
+		'client_id' => '3b9fb7d0184048218f734a1d625f6ba9', 
+		'client_secret' => '61fc57c78bc747edbe2624067f8482ad', 
+		'grant_type' => 'authorization_code', 
+		'redirect_uri' => 'https://photofeed.essdesign.co.uk/instafeed.php', 
+		'code' => $code
+	];
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $uri); // uri
+	curl_setopt($ch, CURLOPT_POST, true); // POST
+	curl_setopt($ch, CURLOPT_POSTFIELDS, $data); // POST DATA
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1); // RETURN RESULT true
+	curl_setopt($ch, CURLOPT_HEADER, 0); // RETURN HEADER false
+	curl_setopt($ch, CURLOPT_NOBODY, 0); // NO RETURN BODY false / we need the body to return
+	curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0); // VERIFY SSL HOST false
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0); // VERIFY SSL PEER false
+	$result = json_decode(curl_exec($ch)); // execute curl
+	echo '<pre>'; // preformatted view
+	
+	// ecit directly the result
+	exit(print_r($result)); 
+	
+	?>
