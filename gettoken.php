8b373bd5f0b28f1b3c9b52300f65decc9e516810
MattScarfe <matt.scarfe@outlook.com>
2018-12-18 16:22:26 +0000

    tweak gettoken.php

--- gettoken.php
+++ gettoken.php
@@ -1,5 +1,36 @@
 <?php require_once("templates/header.php") ?>
 
+
+
+<div class="row">
+	<div class="col-lg-12">
+		<div class="page-header">
+
+			<h1>Get Instagram Access Token</h1>
+		</div>	
+	</div>
+</div>	
+	
+<div class="row">
+	<div class="col">
+	
+	
+		<a href="https://api.instagram.com/oauth/authorize/?client_id=3b9fb7d0184048218f734a1d625f6ba9&redirect_uri=https://photofeed.essdesign.co.uk/gettoken.php&response_type=code" class="btn btn-primary btn-lg">Get your Instagram token</a>
+	</div>
+	<div class="col">
+		<div class="highlight-box">
+			<?php
+			if (isset($_GET['code'])) {
+				echo $_GET['code'];
+			} else {
+				echo "Error-Parameter not set<br />Please click button above.";
+			}
+			?>
+		</div>
+	</div>
+
+</div>
+
 <?php
 
 if (isset($_GET['code'])) {
@@ -35,34 +66,5 @@ $fields = array(
 	
 	$access_token = $result->access_token;
  ?>
-
-<div class="row">
-	<div class="col-lg-12">
-		<div class="page-header">
-
-			<h1>Get Instagram Access Token</h1>
-		</div>	
-	</div>
-</div>	
-	
-<div class="row">
-	<div class="col">
-	
-	
-		<a href="https://api.instagram.com/oauth/authorize/?client_id=3b9fb7d0184048218f734a1d625f6ba9&redirect_uri=https://photofeed.essdesign.co.uk/gettoken.php&response_type=code" class="btn btn-primary btn-lg">Get your Instagram token</a>
-	</div>
-	<div class="col">
-		<div class="highlight-box">
-			<?php
-			if (isset($_GET['code'])) {
-				echo $_GET['code'];
-			} else {
-				echo "Error-Parameter not set<br />Please click button above.";
-			}
-			?>
-		</div>
-	</div>
-
-</div>
 
 <?php require_once("templates/footer.php") ?>
