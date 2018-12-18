8bc4c6d956fe6edebc621ee89cad3f8a4a44cb53
MattScarfe <matt.scarfe@outlook.com>
2018-12-18 15:08:27 +0000

    Finished coding instfeed.php

--- gettoken.php
+++ gettoken.php
@@ -1,35 +1,31 @@
-<!doctype html>
-<html>
-<head>
-<title>Get Instagram Access Token</title>
-
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-<link rel="stylesheet" href="style.css">
-</head>
-
-<body>
-<h1>Get Instagram Access Token</h1>
-
-<a href="https://api.instagram.com/oauth/authorize/?client_id=3b9fb7d0184048218f734a1d625f6ba9&redirect_uri=https://photofeed.essdesign.co.uk/gettoken.php&response_type=code" class="btn btn-primary btn-lg disabled" tabindex="-1" role="button" aria-disabled="true">Get your Instagram token</a>
+<?php require_once("templates/header.php") ?>
 
 
-<div class="highlight-box">
-<?php
-if (isset($_GET['code'])) {
-    echo $_GET['code'];
-} else {
-    echo "Error-Parameter not set<br />Please click button above.";
-}
-?>
+<div class="row">
+	<div class="col-lg-12">
+		<div class="page-header">
+
+			<h1>Get Instagram Access Token</h1>
+		</div>	
+	</div>
+</div>	
+	
+<div class="row">
+	<div class="col">
+		<a href="https://api.instagram.com/oauth/authorize/?client_id=3b9fb7d0184048218f734a1d625f6ba9&redirect_uri=https://photofeed.essdesign.co.uk/instafeed.php&response_type=code" class="btn btn-primary btn-lg">Get your Instagram token</a>
+	</div>
+	<div class="col">
+		<div class="highlight-box">
+			<?php
+			if (isset($_GET['code'])) {
+				echo $_GET['code'];
+			} else {
+				echo "Error-Parameter not set<br />Please click button above.";
+			}
+			?>
+		</div>
+	</div>
+
 </div>
 
-
-
-
-<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-</body>
-
-</html>
+<?php require_once("templates/footer.php") ?>
--- instafeed.php
+++ instafeed.php
@@ -1,8 +1,46 @@
+<?php require_once("templates/header.php") ?>
+
+
+<div class="row">
+
+	<div class="col-lg-12">
+		<div class="page-header">
+			<h1>Instagram Feed</h1>
+		</div>
+	</div>
+
+
+</div>
+
+<div class="row">
+
+	<div class="col-lg-12">
+		<div class="page-header">
+			<h1>Instagram Feed</h1>
+		</div>
+	</div>
+
+
+</div>
+
 <?php
-function text_function() {
-	echo "Wassup";
+
+if (isset($_GET['code'])) {
+	
+	$access_token=$_GET['code'];
+	
+} else {
+	echo "<script>
+alert('Invalid Access Token Get Token. Click OK to get token.');
+window.location.href='gettoken.php';
+</script>"; 
 }
 
-text_function();
+$photo_count=9;
+     
+$json_link="https://api.instagram.com/v1/users/self/media/recent/?";
+$json_link.="access_token={$access_token}&count={$photo_count}";
 
 ?>
+	
+<?php require_once("templates/footer.php") ?>
